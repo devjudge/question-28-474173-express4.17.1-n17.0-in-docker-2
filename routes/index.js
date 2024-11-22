2dfaee9203c835830a00592f2d959a983c8ddd8c
Saleh Sayeem <16103179@iubat.edu>
2024-11-22 13:03:28 +0000

    add 2 number api

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/api/add', function(req, res, next) {
+  const { number1, number2 } = req.body;
+
+  if (typeof number1 === 'number' && typeof number2 === 'number') {
+      const sum = number1 + number2;
+      return res.status(200).json({ sum });
+  } else {
+      return res.status(400).json({ error: 'Invalid input. Please provide two numbers.' });
+  }
+});
+
 module.exports = router;
